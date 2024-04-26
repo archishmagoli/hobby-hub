2a36ccfa1d7a8cb34350299358025b61ae38d6b4
archishmagoli <agoli6@gatech.edu>
2024-04-26 16:54:21 -0400

    more updates

--- src/client.jsx
+++ src/client.jsx
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const VITE_PROJECT_URL = 'https://qpodusqalxtpvhoqofcz.supabase.co';
-const VITE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwb2R1c3FhbHh0cHZob3FvZmN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxMzcyMDUsImV4cCI6MjAyOTcxMzIwNX0.r0zYGjMSTZSMZmKQOQaJib1nOSWdGw8xnpON9_AEgT4';
+const VITE_PROJECT_URL = '';
+const VITE_API_KEY = '';
 
 export const supabase = createClient(VITE_PROJECT_URL, VITE_API_KEY);
