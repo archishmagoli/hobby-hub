ed60703d35c7553921706397924f429ad241c195
archishmagoli <agoli6@gatech.edu>
2024-04-27 00:03:17 -0400

    update env variables

--- src/client.jsx
+++ src/client.jsx
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const VITE_PROJECT_URL = '';
-const VITE_API_KEY = '';
+const VITE_PROJECT_URL = `${import.meta.env.VITE_PROJECT_URL}`;
+const VITE_API_KEY = `${import.meta.env.VITE_API_KEY}`;
 
 export const supabase = createClient(VITE_PROJECT_URL, VITE_API_KEY);
